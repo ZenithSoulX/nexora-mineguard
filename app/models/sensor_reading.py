fb7d84d2d4cde29d88c54493f558619780751637
ZenithSoulX <rahulsingh_ee_25a18025@dtu.ac.in>
2026-08-30 19:27:21 +0530

    models : sensor_readings updated

--- app/models/sensor_reading.py
+++ app/models/sensor_reading.py
@@ -1,20 +1,28 @@
 from datetime import datetime
-
-from sqlalchemy import String, Float, BigInteger, Integer, DateTime
+from sqlalchemy import (
+    Integer,
+    String,
+    Float,
+    Boolean,
+    BigInteger,
+    DateTime
+)
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
-
 from app.database.base import Base
-
 
 class SensorReading(Base):
     __tablename__ = "sensor_readings"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer,primary_key=True)
+    gateway_id: Mapped[str] = mapped_column(String(20))
+    bridge_id: Mapped[str] = mapped_column(String(20))
     node_id: Mapped[str] = mapped_column(String(20))
-    timestamp: Mapped[int] = mapped_column(BigInteger)
+    node_timestamp: Mapped[int] = mapped_column(BigInteger)
+    gateway_received_timestamp: Mapped[int] = mapped_column(BigInteger)
     tilt_x: Mapped[float] = mapped_column(Float)
     tilt_y: Mapped[float] = mapped_column(Float)
-    vibration: Mapped[float] = mapped_column(Float)
-    flex_raw : Mapped[float] = mapped_column(Float)
-    crack_ok : Mapped[bool] = mapped_column(Integer, default=0)
+    vib_rms: Mapped[float] = mapped_column(Float)
+    flex_raw: Mapped[int] = mapped_column(Integer)
+    crack_ok: Mapped[bool] = mapped_column(Boolean)
+    rssi: Mapped[float | None] = mapped_column(Float,nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True),server_default=func.now())
